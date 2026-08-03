f8c8f67a607727b2fe8ee7c9564284160c5972f1
Rekha <9392rekha@gmail.com>
2026-08-03 16:09:38 +0530

    Day2chnages

---
+++ track/javatrackpractise/Javapractise.java
@@ -0,0 +1,14 @@
+
+public class Javapractise {
+
+    public static void main(String[] args) {
+        String learnerName = "Ananya";
+        int completedProblems = 25;
+        double practiseHours = 2.5;
+        boolean projectCompleted = true;
+        System.out.println("Learner:" + learnerName);
+        System.out.println("problems Completed:" + completedProblems);
+        System.out.println("Practise hours" + practiseHours);
+        System.out.println("Project Completed:" + projectCompleted);
+    }
+}
