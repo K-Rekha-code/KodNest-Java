cce8c6a51db517b47ec3e4a9568c1251aa13d046
Rekha <9392rekha@gmail.com>
2026-08-04 13:27:40 +0530

    Day3chnages

---
+++ track/MO/Module2/Topic2Practise1.java
@@ -0,0 +1,16 @@
+
+public class Topic2Practise {
+
+    public static void main(String[] args) {
+        int marks = 72;
+        int attendance = 80;
+        boolean projectCompleted = true;
+
+        boolean marksEligible = marks >= 60;
+        boolean attendanceEligible = attendance >= 75;
+        boolean academicEligible = marksEligible && attendanceEligible;
+        boolean placementEligible = academicEligible && projectCompleted;
+
+        System.out.println(placementEligible);
+    }
+}
