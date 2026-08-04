0d109710e41a39d661a44961f093403012748750
Rekha <9392rekha@gmail.com>
2026-08-04 12:31:39 +0530

    Day3chnages

---
+++ track/MO2/TO1/STO1/PO1/ClassPractise1.java
@@ -0,0 +1,16 @@
+
+class ClassPractise1 {
+
+    public static void main(String[] args) {
+        int num = 25;
+        if (num > 0) {
+            System.out.println("Number is positive");
+        } else if (num < 0) {
+            System.out.println("Number is negative");
+
+        } else {
+            System.out.println("Number is zero");
+        }
+
+    }
+}
