a2f40fa6a6acedaa3f5af7c7b22d7aef007b827b
Rekha <9392rekha@gmail.com>
2026-08-06 11:49:22 +0530

    day6changes

---
+++ track/MO2/TO1/STO1/PO1/Program6.java
@@ -0,0 +1,11 @@
+public Program6{
+    public static void main(String[] args)
+    {
+       int i=1;
+       while(i<=4)
+       {
+        System.out.println(i);
+        i++;
+       } 
+    }
+}
