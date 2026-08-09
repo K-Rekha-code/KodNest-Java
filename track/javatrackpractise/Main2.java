f98ae0d1de0633753a2e5dc93fb5d2bc834cb53a
Rekha <9392rekha@gmail.com>
2026-08-09 20:40:56 +0530

    day7changes

---
+++ track/javatrackpractise/Main2.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        int practiceDays = 5;
+        int revisionDay = 3;
+        int sessionsPerDay = 2;
+        int finalDaySessionLimit = 1;
+        int targetSessions = 7;
+        int completedSessions = 0;
+
+        for (int day = 1; day <= practiceDays; day++) {
+            if (day == revisionDay) {
+                continue;
+            }
+
+            for (int session = 1; session <= sessionsPerDay; session++) {
+                if (day == practiceDays && session > finalDaySessionLimit) {
+                    break;
+                }
+
+                System.out.println("Day " + day + " - Session " + session);
+                completedSessions++;
+            }
+        }
+
+        System.out.println("Completed Sessions: " + completedSessions);
+
+        if (completedSessions >= targetSessions) {
+            System.out.println("Target Achieved");
+        } else {
+            System.out.println("Target Pending");
+        }
+    }
+}
