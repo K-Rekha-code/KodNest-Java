346d06c0e8398b3c39488f9677aaa642bb809f79
Rekha <9392rekha@gmail.com>
2026-08-09 16:45:34 +0530

    day7changes

---
+++ track/MO2/TO1/STO1/PO1/Program10.java
@@ -0,0 +1,15 @@
+class Program10{
+    public static void main(String[] args)
+    {
+        for(int i=1;i<=3;i++)
+        {
+            for(int j=1;j<=2;j++)
+            {
+                if(i==2)
+                {
+                    continue;   
+                }
+                System.out.println(i);
+            }
+    }
+}
