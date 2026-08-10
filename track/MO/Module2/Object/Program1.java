97a2dba0e2c1cbaf4b12c212296189edaf8739da
Rekha <9392rekha@gmail.com>
2026-08-10 13:07:54 +0530

    day8changes

---
+++ track/MO/Module2/Object/Program1.java
@@ -0,0 +1,15 @@
+class Program1{
+    int roll;
+    string name;
+    double height;
+    void run()
+    {
+        System.out.println("running");
+
+    }
+    void sleep()
+    {
+        System.out.println("Sleeping")
+    }
+    
+}
