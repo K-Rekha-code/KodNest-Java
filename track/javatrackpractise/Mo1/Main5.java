9dbb8fd1857bf50694cb50113381e47b72e78d3f
Rekha <9392rekha@gmail.com>
2026-08-11 15:02:38 +0530

    day9changes

---
+++ track/javatrackpractise/Mo1/Main5.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Create one StudentProgress object
+        StudentProgress student = new StudentProgress();
+
+        // Read and store student data
+        student.name = scanner.next();
+        student.completedLessons = scanner.nextInt();
+        student.totalLessons = scanner.nextInt();
+
+        // Calculate and store percentage
+        student.percentage = student.completedLessons * 100 / student.totalLessons;
+
+        // Print progress card
+        System.out.println("Student: " + student.name);
+        System.out.println("Progress: " + student.completedLessons + "/" + student.totalLessons);
+        System.out.println("Completion: " + student.percentage + "%");
+    }
+}
+
+
+class StudentProgress {
+    String name;
+    int completedLessons;
+    int totalLessons;
+    int percentage;
+}
