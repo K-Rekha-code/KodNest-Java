545f9ff28d912e735ac9d74c7a0350021fab8854
Rekha <9392rekha@gmail.com>
2026-08-12 12:01:23 +0530

    day10changes

---
+++ track/MO/Module2/Shadowing.java
@@ -0,0 +1,23 @@
+ public class Shadowing{
+    Public static void main(String args[])
+    Student s1=new Studen();
+    
+    s1.input("Raja",18,5.5);
+   
+    s1.display();
+}
+}
+class Student{
+    String name;
+    int age;
+    double height;
+    void input(String name,int age,double height)
+    {
+        this.name=name;
+        this.age=age;
+        this.height=height;
+    }
+    void display(){
+        System.out.println(name+" "+age+" "+height);
+    }   
+}
